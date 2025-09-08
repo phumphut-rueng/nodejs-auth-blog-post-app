b594116b8ed487566729d4719025d6b257ee207b
Phumphut <phumphut.rueng@gmail.com>
2025-09-08 16:21:28 +0700

    feat: Building JWT Authentication (Part 1)

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 const AuthContext = React.createContext();
 
 function AuthProvider(props) {
+
   const [state, setState] = useState({
     loading: null,
     error: null,
     user: null,
   });
 
-  const login = () => {
+  const navigate = useNavigate();
+  const login = async(data) => {
+    const result = await axios.post("http://localhost:4000/auth/login", data);
+    const token = result.data.token;
+    localStorage.setItem("token", token);
+    const userDataFromToken = jwtDecode(token);
+    setState({...state, user: userDataFromToken });
+    navigate("/");
     // 🐨 Todo: Exercise #4
     //  ให้เขียน Logic ของ Function `login` ตรงนี้
     //  Function `login` ทำหน้าที่สร้าง Request ไปที่ API POST /login
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
   };
 
-  const register = () => {
+  const register = async (data) => {
+    await axios.post("http://localhost:4000/auth/register", data);
+    navigate("/login");
     // 🐨 Todo: Exercise #2
     //  ให้เขียน Logic ของ Function `register` ตรงนี้
     //  Function register ทำหน้าที่สร้าง Request ไปที่ API POST /register
--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,11 +1,62 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {  
+    const user = {
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    };
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    const result = await db.collection("users");
+    await result.insertOne(user);
+    res.status(201).json({ 
+        message: "User has been created successfully"  
+    });
+
+});
 
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
+authRouter.post("/login", async (req, res) => {
+    const user = await db.collection("users").findOne({ 
+        username: req.body.username 
+    });
+    if (!user) {
+        return res.status(401).json({
+            message: "username not found"
+        });
+    }
+    const validPassword = await bcrypt.compare(
+        req.body.password, 
+        user.password
+    );
+
+    if (!validPassword) {
+        return res.status(400).json({
+            message: "Invalid password"
+        });
+    }
+    const token = jwt.sign(
+        { id: user._id , firstName: user.firstName, lastName: user.lastName },
+        process.env.SECRET_KEY,
+        { 
+            expiresIn: "900000" 
+        }
+    );
+    return res.json({
+        message: "Login successful",
+        token,
+    });
+});
+
 
 export default authRouter;
