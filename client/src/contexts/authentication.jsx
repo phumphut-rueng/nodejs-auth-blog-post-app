3e5902a2fb238fba0806fbcdb11913ce391c7338
Phumphut <phumphut.rueng@gmail.com>
2025-09-09 16:03:54 +0700

    feat:  Building JWT Authentication (Part 2)

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -40,6 +40,9 @@ function AuthProvider(props) {
     // 🐨 Todo: Exercise #7
     //  ให้เขียน Logic ของ Function `logout` ตรงนี้
     //  Function logout ทำหน้าที่ในการลบ JWT Token ออกจาก Local Storage
+    localStorage.removeItem("token");
+    setState({ ...state, user: null });
+    navigate("/login");
   };
 
   const isAuthenticated = Boolean(localStorage.getItem("token"));
